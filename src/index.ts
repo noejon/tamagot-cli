6df5e053dff8617fb44f89fe7e41c2899bc16577
Jonathan Noe <j.noe@nib.com.au>
2020-06-29 20:49:33 +1000

    Dockerising what I think can be dockerised

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 #!/usr/bin/env node
 import Tamagotchi from './classes/tamagotchi';
 
 const tamagotchi = new Tamagotchi();
 
 tamagotchi.run();
-=======
-console.log('Hello, world!');
->>>>>>> Revert "Dockerising what I think can be dockerised"
